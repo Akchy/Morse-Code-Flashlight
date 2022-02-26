a3b9e7250486d4e035dca63ce836cbcf38e320cc
jayasankar J P <scicopath01@gmail.com>
2022-02-26 19:38:48 +0530

    Add morscode To txt

--- lib/functions/mosCode.dart
+++ lib/functions/mosCode.dart
@@ -1,6 +1,92 @@
 import 'package:flutter/cupertino.dart';
 
 class MosCode {
+  String charatorGenerator(String charator) {
+    switch (charator) {
+      case ".-":
+        return "A";
+      case "_...":
+        return "B";
+      case "_._.":
+        return "C";
+      case "_..":
+        return "D";
+      case ".":
+        return "E";
+      case ".._.":
+        return "F";
+      case "__.":
+        return "G";
+      case "....":
+        return "H";
+      case "..":
+        return "I";
+      case ".___":
+        return "J";
+      case "_._":
+        return "K";
+      case "._..":
+        return "L";
+      case "__":
+        return "M";
+      case "-.":
+        return "N";
+      case "___":
+        return "O";
+      case ".__.":
+        return "P";
+      case "__._":
+        return "Q";
+      case ".-.":
+        return "R";
+      case "...":
+        return "S";
+      case "-":
+        return "T";
+      case ".._":
+        return "U";
+      case "..._":
+        return "V";
+      case ".__":
+        return "W";
+      case "_.._":
+        return "X";
+      case "_.__":
+        return "Y";
+      case "__..":
+        return "Z";
+      case "._._._":
+        return ".";
+      case "__..__":
+        return ",";
+      case "..__..":
+        return "?";
+      case ".____":
+        return "1";
+      case "..___":
+        return "2";
+      case "...__":
+        return "3";
+      case "...._":
+        return "4";
+      case ".....":
+        return "5";
+      case "_....":
+        return "6";
+      case "__...":
+        return "7";
+      case "___..":
+        return "8";
+      case "____.":
+        return "9";
+      case "_____":
+        return "0";
+      default:
+        return " ";
+    }
+    return " ";
+  }
+
   String mosCodeGenerator(String charator) {
     charator = charator.toLowerCase();
     String mosCode = "";
